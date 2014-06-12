1e17d10005da7be8313b1bfd8bc8628373cd86ff
Lachlan Priest <lachlanpriest@gmail.com>
2014-06-12 18:05:20 +0800

    Added more store calls

--- lib/magento/store.rb
+++ lib/magento/store.rb
@@ -1,7 +1,12 @@
 module MagentoAPI
   class Store < Base
-    def self.current
+    def self.info
       MagentoAPI::Base.connection.call('core_magento.info')
     end
+
+    def self.current
+      MagentoAPI::Base.connection.call('store.list')
+    end
+
   end
 end
